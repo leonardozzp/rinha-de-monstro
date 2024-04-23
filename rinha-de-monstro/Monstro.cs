017a690897e11e61e86c925af3982348f165fb29
Leonardo Henrique de Oliveira <leonardooliveira97@outlook.com>
2024-04-23 20:19:32 -0300

    commit 1

---
+++ rinha-de-monstro/Monstro.cs
@@ -0,0 +1,44 @@
+﻿class Monstro
+{
+    public string Nome { get; }
+    public string TipoPrincipal { get; }
+    public int Forca { get; }
+    public int Defesa { get; }
+    public int Nivel { get; private set; }
+    public int ExperienciaTotal { get; private set; }
+
+    public Monstro(string nome, string tipo, int forca, int defesa, int nivel, int experiencia)
+    {
+        Nome = nome;
+        TipoPrincipal = tipo;
+        Forca = forca;
+        Defesa = defesa;
+        Nivel = nivel <= 0 ? Nivel = 1 : Nivel += nivel;
+        this.ExibirMonstro();
+        Console.WriteLine(this.AdicionarExperiencia(experiencia));
+    }
+
+    public string AdicionarExperiencia(int experiencia)
+    {
+
+        if (experiencia > 0)
+        {
+            this.ExperienciaTotal = ExperienciaTotal + experiencia;
+            return $"{Nome} está no nível {Nivel} e possui {ExperienciaTotal}";
+        }
+        else
+        {
+            return "Nenhuma experiência adquirida";
+        }
+    }
+
+    public void ExibirMonstro()
+    {
+        Console.WriteLine( $"Nome: {Nome} \n" +
+            $"Tipo:{TipoPrincipal} \n" +
+            $"Força:{Forca}\n" +
+            $"Defesa:{Defesa}\n" +
+            $"Nível:{Nivel} \n" +
+            $"Experiência:{ExperienciaTotal}");
+    }
+}
---
+++ rinha-de-monstro/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Monstro monstro = new Monstro("Pikachu", "Raio", 4, 2, 0, 0);
+Console.ReadKey();
